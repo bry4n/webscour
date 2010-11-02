bd179f14cf27a168cac819fc2109c4789591bc9b
bry4n <bryann83@gmail.com>
2010-11-01 20:48:41 -0500

    add more examples in spec

--- spec/webscour_spec.rb
+++ spec/webscour_spec.rb
@@ -8,9 +8,13 @@ describe "WebScour" do
     <title>Hello World</title>
   </head>
   <body>
-    <p>
-      This is testing.
-    </p>
+    <p>This is testing.</p>
+
+    <ul>
+      <li>A</li>
+      <li>B</li>
+      <li>C</li>
+    </ul>
   </body>
 </html>
     HTML
@@ -19,28 +23,31 @@ describe "WebScour" do
 
   it "should have data" do
     @data.should_not be_nil
+    @data.should_not be_empty
   end
 
   it "should have content" do
     resp = Faraday.get "http://www.example.com/"
-    resp.body.should_not be_nil
+    resp.body.should_not be_empty
+    resp.body.should == @data
   end
+
   it "should have title" do
     html = WebScour.parse("http://www.example.com/") do
+      
       element :title
+      
+      element './/body/p' => :body
+
+      elements :ul => :list do
+        element:li => :text
+      end
+
     end
+
     html.title.should == "Hello World"
+    html.body.should =~ /This is testing\./
+    html.list.should be_kind_of(Array)
+    html.list.first.text.should == "A"
   end
 end
-
-__END__
-<html>
-<head>
-<title>Hello World</title>
-</head>
-<body>
-<p>
-This is testing.
-  </p>
-</body>
-</html>
